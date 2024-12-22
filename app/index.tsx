a1c838b9c8443c0ae55489b2eb89065c7c54375a
Akash M Nandan <akash003nandan@gmail.com>
2024-12-22 15:57:15 +0530

    index file and images of pokemon images

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from 'react-native'
 
-export default function Index() {
+export default function index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  }
+})
