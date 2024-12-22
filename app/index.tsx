e944f4841decc5eb9329f4173eb1734e4a251c2f
Akash M Nandan <akash003nandan@gmail.com>
2024-12-22 16:25:11 +0530

    more data added

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,59 @@
-import { View, Text, StyleSheet, Platform, SafeAreaView } from 'react-native'
-import PokemonCards from '@/Components/PokemonCards'
+import { StyleSheet, SafeAreaView, Platform, ScrollView } from "react-native";
+import PokemonCard from "@/Components/PokemonCards";
 
-export default function index() {
-
+export default function App() {
   const charmanderData = {
     name: "Charmander",
-    image: require("@/assets/images/charmander.png"),
+    image: require("@/assets/images/charmander.png"), 
     type: "Fire",
     hp: 39,
     moves: ["Scratch", "Ember", "Growl", "Leer"],
     weaknesses: ["Water", "Rock"],
   };
 
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("@/assets/images/squirtle.png"), 
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("@/assets/images/bulbasaur.png"), 
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("@/assets/images/pikachu.png"), 
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <PokemonCards {...charmanderData} />
+      <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...bulbasaurData} />
+        <PokemonCard {...pikachuData} />
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingTop: Platform.OS === 'android' ? 25 : 0
-  }
-})
+    backgroundColor: "#F5F5F5",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+});
