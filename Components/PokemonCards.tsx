4248ba6fa7468262b0cfabe44c7c3df82dd7a588
Akash M Nandan <akash003nandan@gmail.com>
2024-12-22 16:01:43 +0530

    Pokemon Card components Created

---
+++ Components/PokemonCards.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PokemonCards() {
+  return (
+    <View>
+      <Text>Pokemon Cards</Text>
+    </View>
+  )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
+import PokemonCards from '@/Components/PokemonCards'
 
 export default function index() {
   return (
     <View style={styles.container}>
+      <PokemonCards />
     </View>
   )
 }
@@ -11,5 +13,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 25 : 0
   }
 })
