d779f3eb6c0ec455aab265928b5c36743ac1ebef
Akash M Nandan <akash003nandan@gmail.com>
2024-12-22 16:32:04 +0530

    header dissabled

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen 
+      name="index"
+      options={{
+        title: "Home",
+        headerShown: false,
+      }}
+      />
+  </Stack>;
 }
