1e8f02c71aae21727a95e42bc1aa38a51467836c
Akash M Nandan <akash003nandan@gmail.com>
2024-12-22 16:08:02 +0530

    pokemon acard styling added

--- Components/PokemonCards.tsx
+++ Components/PokemonCards.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import React from 'react'
 
 export default function PokemonCards() {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>Pokemon Cards</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'white',
+        borderRadius : 16,
+        borderWidth: 2,
+        padding: 16,
+        margin: 16,
+        ...Platform.select({
+            ios: {
+                shadowColor: '#333',
+                shadowOffset: { width: 2, height: 2 },
+                shadowOpacity: 0.3,
+                shadowRadius: 4
+            },
+            android: {
+                elevation: 5
+            }
+        })
+    }
+})
